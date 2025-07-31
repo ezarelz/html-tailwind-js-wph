0d11fb2b101d8ac86ccae1beaeb7f0f3603b15f2
ezarelz <manggala.proflight@gmail.com>
2025-07-31 15:42:39 +0700

    feat: setup basic dark mode toggle using Tailwind utility

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  darkMode: 'class', // aktifkan dark mode berbasis class
+  content: ['./index.html'], // sesuaikan dengan lokasi file HTML-mu
   theme: {
     extend: {},
   },
